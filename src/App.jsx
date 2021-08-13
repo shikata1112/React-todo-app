debc82d144aeb7b4f5cc9f48cf187f36da73b4d0
四方 秀 <shu.42111227@gmail.com>
2021-08-13 23:19:15 +0900

    todoを入力し追加するフォームをコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { InputTodo } from "./components/InputTodo";
 import "./styles.css";
 
 export const App = () => {
@@ -41,14 +42,11 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
 
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
